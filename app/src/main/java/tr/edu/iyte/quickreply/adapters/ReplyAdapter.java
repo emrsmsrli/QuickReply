df2e6f4beb894fa29a69f24c30d52c69670ab6b8
Emre <emrsmsrli1@gmail.com>
2017-06-18 01:26:59 +0300

    we're changing baseAdapter with recyclerView.Adapter

--- app/src/main/java/tr/edu/iyte/quickreply/adapters/ReplyAdapter.java
+++ app/src/main/java/tr/edu/iyte/quickreply/adapters/ReplyAdapter.java
@@ -1,43 +1,69 @@
 package tr.edu.iyte.quickreply.adapters;
 
-import android.annotation.SuppressLint;
-import android.content.Context;
+import android.graphics.Color;
+import android.support.v4.view.MotionEventCompat;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.TextView;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import tr.edu.iyte.quickreply.QuickReplyTile;
 import tr.edu.iyte.quickreply.R;
+import tr.edu.iyte.quickreply.interfaces.ReplyItemTouchHelperAdapter;
+import tr.edu.iyte.quickreply.interfaces.ViewHolderInteractionListener;
+import tr.edu.iyte.quickreply.interfaces.OnReplyInteractedListener;
+import tr.edu.iyte.quickreply.interfaces.OnStartDragListener;
 
-public class ReplyAdapter extends BaseAdapter {
-    private static class ViewHolder {
-        private TextView textView;
-        private View replyLayout;
+public class ReplyAdapter
+        extends RecyclerView.Adapter<ReplyAdapter.ViewHolder>
+        implements ReplyItemTouchHelperAdapter {
 
-        private ViewHolder(View v) {
-            textView = (TextView) v.findViewById(R.id.reply_text);
-            replyLayout = v.findViewById(R.id.reply_layout);
-        }
-    }
+    private final List<String> replies;
+    private final OnStartDragListener sdListener;
+    private final OnReplyInteractedListener riListener;
 
-    private final List<String> data;
-    private final LayoutInflater inflater;
-    private final View.OnTouchListener tListener;
-
-    public ReplyAdapter(Context c, View.OnTouchListener tListener, ArrayList<String> data) {
-        this.data = data;
-        this.tListener = tListener;
-        inflater = (LayoutInflater) c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    public ReplyAdapter(List<String> replies, OnStartDragListener sdListener, OnReplyInteractedListener riListener) {
+        super();
+        this.replies = replies;
+        this.sdListener = sdListener;
+        this.riListener = riListener;
     }
 
     @Override
-    public String getItem(int position) {
-        return data.get(position);
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.reply, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.textView.setText(replies.get(position));
+
+        holder.itemView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(MotionEventCompat.getActionMasked(event) == MotionEvent.ACTION_DOWN) {
+                    sdListener.onStartDrag(holder);
+                }
+                return false;
+            }
+        });
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                riListener.onReplySelected(holder.textView.getText().toString());
+            }
+        });
+    }
+
+    @Override
+    public void onReplyDismissed(int position) {
+        remove(position);
+        riListener.onReplyDismissed();
     }
 
     @Override
@@ -45,36 +71,40 @@ public class ReplyAdapter extends BaseAdapter {
         return position;
     }
 
-    @SuppressLint("InflateParams")
     @Override
-    public View getView(final int position, View convertView, final ViewGroup parent) {
-        final ViewHolder v;
-        if(convertView == null) {
-            convertView = inflater.inflate(R.layout.reply, null);
-            v = new ViewHolder(convertView);
-            convertView.setTag(v);
-        } else
-            v = (ViewHolder) convertView.getTag();
-
-        v.textView.setText(data.get(position));
-        v.replyLayout.setOnTouchListener(tListener);
-
-        return convertView;
+    public int getItemCount() {
+        return replies.size();
     }
 
-    @Override
-    public int getCount() {
-        return data.size();
+    public void add(String reply) {
+        replies.add(reply);
+        notifyItemInserted(replies.size() - 1);
     }
 
-    public void add(String item) {
-        data.add(item);
-        notifyDataSetChanged();
+    private void remove(int idx) {
+        QuickReplyTile.removeReply(replies.get(idx));
+        replies.remove(idx);
+        notifyItemRemoved(idx);
     }
 
-    public void remove(int idx) {
-        QuickReplyTile.removeReply(data.get(idx));
-        data.remove(idx);
-        notifyDataSetChanged();
+    public static class ViewHolder
+            extends RecyclerView.ViewHolder
+            implements ViewHolderInteractionListener {
+        private TextView textView;
+
+        private ViewHolder(View v) {
+            super(v);
+            textView = (TextView) v.findViewById(R.id.reply_text);
+        }
+
+        @Override
+        public void onInteractionStart() {
+            itemView.setBackgroundColor(Color.LTGRAY);
+        }
+
+        @Override
+        public void onInteractionEnd() {
+            itemView.setBackgroundColor(0);
+        }
     }
 }
