5aac7290082485b0676c479a6e20172d7965e1d3
Emre <emrsmsrli1@gmail.com>
2017-06-24 12:38:51 +0300

    create ruleAdapter, will be PreferencesActivity

---
+++ app/src/main/java/tr/edu/iyte/quickreply/adapters/RuleAdapter.java
@@ -0,0 +1,33 @@
+package tr.edu.iyte.quickreply.adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class RuleAdapter extends RecyclerView.Adapter<RuleAdapter.ViewHolder> {
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+
+    public RuleAdapter() {
+
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
