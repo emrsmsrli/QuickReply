76f811720a5a0d71521b5f9b21aa04c12ea28e7b
Emre <emrsmsrli1@gmail.com>
2017-06-24 19:28:38 +0300

    implement ruleAdapter with basic funcs

--- app/src/main/java/tr/edu/iyte/quickreply/adapters/RuleAdapter.java
+++ app/src/main/java/tr/edu/iyte/quickreply/adapters/RuleAdapter.java
@@ -1,33 +1,72 @@
 package tr.edu.iyte.quickreply.adapters;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
 
-public class RuleAdapter extends RecyclerView.Adapter<RuleAdapter.ViewHolder> {
+import tr.edu.iyte.quickreply.R;
+import tr.edu.iyte.quickreply.helper.Rule;
+
+public class RuleAdapter
+        extends RecyclerView.Adapter<RuleAdapter.ViewHolder> {
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        public ViewHolder(View itemView) {
+        private final TextView time;
+        private final TextView days;
+        private final ImageButton cancelRule; // TODO remove
+
+        private ViewHolder(View itemView) {
             super(itemView);
+            time = (TextView) itemView.findViewById(R.id.time);
+            days = (TextView) itemView.findViewById(R.id.days);
+            cancelRule = (ImageButton) itemView.findViewById(R.id.cancel);
         }
     }
 
-    public RuleAdapter() {
+    private List<Rule> rules = new ArrayList<>();
 
-    }
+    public RuleAdapter() {}
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.rule, parent, false));
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.time.setText(rules.get(position).getTimeString(holder.itemView.getContext()));
+        holder.days.setText(rules.get(position).getDaysString());
+        
+        holder.cancelRule.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: 24/06/2017
+            }
+        });
+    }
+    
+    public void add(Rule rule) {
+        rules.add(rule);
+        notifyItemInserted(rules.size() - 1);
+    }
 
+    public void add(List<Rule> rules) {
+        this.rules.addAll(rules);
+        notifyItemRangeInserted(0, rules.size());
+    }
+    
+    public void remove(int index) {
+        rules.remove(index);
+        notifyItemRemoved(index);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return rules.size();
     }
 }
