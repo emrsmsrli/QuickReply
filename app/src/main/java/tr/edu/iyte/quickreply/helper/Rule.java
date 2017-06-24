d5f62cf896ac7cc74de1553659f281fe270bc16b
Emre <emrsmsrli1@gmail.com>
2017-06-24 19:29:14 +0300

    extend rule pojo to its basic funcs

--- app/src/main/java/tr/edu/iyte/quickreply/helper/Rule.java
+++ app/src/main/java/tr/edu/iyte/quickreply/helper/Rule.java
@@ -1,4 +1,45 @@
 package tr.edu.iyte.quickreply.helper;
 
+import android.content.Context;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+import tr.edu.iyte.quickreply.R;
+
 public class Rule {
+    private String id;
+    private long fromTime;
+    private long toTime;
+    private List<String> days;
+
+    public Rule(String id, long fromTime, long toTime, List<String> days) {
+        this.id = id;
+        this.fromTime = fromTime;
+        this.toTime = toTime;
+        this.days = new ArrayList<>(days);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public List<String> getDays() {
+        return days;
+    }
+
+    public String getDaysString() {
+        String d = days.toString();
+        return d.substring(1, d.length() - 1); // remove '[' and ']'
+    }
+
+    public String getTimeString(Context c) { // TODO: 24/06/2017 check if format is true?
+        SimpleDateFormat s = new SimpleDateFormat("HH:mm", Locale.getDefault());
+        return s.format(new Date(fromTime))
+                + c.getString(R.string.clock_seperator)
+                + s.format(new Date(toTime));
+    }
 }
