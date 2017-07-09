f7bac091fc59ba008e8dd167cfb2bd0709f5b469
Emre <emrsmsrli1@gmail.com>
2017-07-09 13:14:42 +0300

    convert Rule java class to kotlin class

---
+++ app/src/main/java/tr/edu/iyte/quickreply/rules/Rule.kt
@@ -0,0 +1,35 @@
+package tr.edu.iyte.quickreply.rules
+
+import android.content.Context
+import tr.edu.iyte.quickreply.R
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+data class Rule(val id: String = "",
+                var reply: String = "",
+                var startTime: Long = 0,
+                var endTime: Long = 0,
+                val days: MutableList<String> = ArrayList<String>(),
+                var isEnabled: Boolean = false) {
+    private val FORMATTER = SimpleDateFormat("HH:mm", Locale.getDefault())
+
+    fun getDaysString(c: Context): String {
+        if (days.size == 1)
+            return days[0]
+
+        return with(StringBuilder(32)) {
+            append(days[0])
+            for (i in 1..days.size - 1 - 1)
+                append(", ").append(days[i])
+            append(" ").append(c.getString(R.string.and))
+            append(" ").append(days[days.size - 1])
+        }.toString()
+    }
+
+    fun getTimeString(c: Context) =
+            "${FORMATTER.format(Date(startTime))} ${c.getString(R.string.clock_seperator)} ${FORMATTER.format(Date(endTime))}"
+
+    fun canClashWith(other: Rule) = this.endTime < other.startTime
+            || other.endTime < this.startTime
+}
