d8be8f2faed71d889e7bc35e46bfd9718846bbac
Emre <emrsmsrli1@gmail.com>
2017-07-09 16:07:31 +0300

    move more constants to Constants class

--- app/src/main/java/tr/edu/iyte/quickreply/helper/Constants.kt
+++ app/src/main/java/tr/edu/iyte/quickreply/helper/Constants.kt
@@ -5,4 +5,9 @@ object Constants {
     const val SHARED_PREF_RULE_ENABLE_KEY = "tr.edu.iyte.quickreply.sharedprefs.ruleenable"
     const val SHARED_PREF_RULE_DND_ENABLE_KEY = "tr.edu.iyte.quickreply.sharedprefs.dndenable"
     const val SHARED_PREF_RULE_DND_REPLY_KEY = "tr.edu.iyte.quickreply.sharedprefs.dndreply"
+
+    const val SHARED_PREF_REPLIES_KEY = "tr.edu.iyte.quickreply.sharedprefs.replies"
+    const val SHARED_PREF_CURRENT_REPLY_KEY = "tr.edu.iyte.quickreply.sharedprefs.currentreply"
+    const val SHARED_PREF_PERMISSIONS_KEY = "tr.edu.iyte.quickreply.sharedprefs.perms"
+    const val SHARED_PREF_REPLY_COUNT_KEY = "tr.edu.iyte.quickreply.sharedprefs.replycount"
 }
