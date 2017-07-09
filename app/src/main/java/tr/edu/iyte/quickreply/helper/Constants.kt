7e0fcce225688ea984ce973e0f9152d8b47a38a6
Emre <emrsmsrli1@gmail.com>
2017-07-09 13:16:36 +0300

    separate constants from QuickReplyTiles class

---
+++ app/src/main/java/tr/edu/iyte/quickreply/helper/Constants.kt
@@ -0,0 +1,8 @@
+package tr.edu.iyte.quickreply.helper
+
+object Constants {
+    const val SHARED_PREF_KEY = "tr.edu.iyte.quickreply.sharedprefs"
+    const val SHARED_PREF_RULE_ENABLE_KEY = "tr.edu.iyte.quickreply.sharedprefs.ruleenable"
+    const val SHARED_PREF_RULE_DND_ENABLE_KEY = "tr.edu.iyte.quickreply.sharedprefs.dndenable"
+    const val SHARED_PREF_RULE_DND_REPLY_KEY = "tr.edu.iyte.quickreply.sharedprefs.dndreply"
+}
