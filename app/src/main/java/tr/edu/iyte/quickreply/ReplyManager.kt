e799307bc77237c135611717ff25f258a65edc92
Emre <emrsmsrli1@gmail.com>
2017-07-10 20:23:27 +0300

    add logs to replyManager

--- app/src/main/java/tr/edu/iyte/quickreply/ReplyManager.kt
+++ app/src/main/java/tr/edu/iyte/quickreply/ReplyManager.kt
@@ -3,9 +3,11 @@ package tr.edu.iyte.quickreply
 import android.content.Context
 import android.content.SharedPreferences
 import android.util.Log
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
 import tr.edu.iyte.quickreply.helper.Constants
 
-object ReplyManager {
+object ReplyManager : AnkoLogger {
     private const val TAG = "ReplyManager"
 
     const val DEFAULT_REPLY = ""
@@ -16,16 +18,28 @@ object ReplyManager {
 
     var currentReply: String
         get() = prefs.getString(Constants.SHARED_PREF_CURRENT_REPLY_KEY, DEFAULT_REPLY)
-        set(value) { prefs.edit().putString(Constants.SHARED_PREF_CURRENT_REPLY_KEY, value).apply(); Log.i(TAG, "Selected reply: $value") }
+        set(value) {
+            prefs.edit().putString(Constants.SHARED_PREF_CURRENT_REPLY_KEY, value).apply()
+            info("Selected reply: $value")
+        }
     var replyCount: Int
         get() = prefs.getInt(Constants.SHARED_PREF_REPLY_COUNT_KEY, DEFAULT_REPLY_COUNT)
-        set(value) = prefs.edit().putInt(Constants.SHARED_PREF_REPLY_COUNT_KEY, value).apply()
+        set(value) {
+            prefs.edit().putInt(Constants.SHARED_PREF_REPLY_COUNT_KEY, value).apply()
+            info("Reply count: $value")
+        }
     val replies: Set<String>
         get() = prefs.getStringSet(Constants.SHARED_PREF_REPLIES_KEY, defaultReplies)
 
-    fun addReply(reply: String) = prefs.edit().putStringSet(Constants.SHARED_PREF_REPLIES_KEY, replies.plus(reply)).apply()
-    fun removeReply(reply: String) = prefs.edit().putStringSet(Constants.SHARED_PREF_REPLIES_KEY, replies.minus(reply)).apply()
     fun hasNoReply() = replies.isEmpty()
+    fun addReply(reply: String) {
+        prefs.edit().putStringSet(Constants.SHARED_PREF_REPLIES_KEY, replies.plus(reply)).apply()
+        info("New reply added: $reply")
+    }
+    fun removeReply(reply: String) {
+        prefs.edit().putStringSet(Constants.SHARED_PREF_REPLIES_KEY, replies.minus(reply)).apply()
+        info("Reply deleted: $reply")
+    }
 
     fun init(context: Context) {
         prefs = context.getSharedPreferences(Constants.SHARED_PREF_KEY, Context.MODE_PRIVATE)
