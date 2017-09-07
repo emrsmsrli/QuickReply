cdac25198d40805bf69c5d5f4216b6f18f5193cf
Emre <emrsmsrli1@gmail.com>
2017-09-07 22:32:49 +0300

    remove unused imports

--- app/src/main/java/tr/edu/iyte/quickreply/activities/SettingsActivity.kt
+++ app/src/main/java/tr/edu/iyte/quickreply/activities/SettingsActivity.kt
@@ -1,17 +1,7 @@
 package tr.edu.iyte.quickreply.activities
 
 import android.app.Activity
-import android.app.AlertDialog
 import android.os.Bundle
-import android.preference.Preference
-import android.preference.PreferenceFragment
-import android.widget.ArrayAdapter
-import org.jetbrains.anko.AnkoLogger
-import org.jetbrains.anko.info
-import org.jetbrains.anko.verbose
-
-import tr.edu.iyte.quickreply.R
-import tr.edu.iyte.quickreply.ReplyManager
 import tr.edu.iyte.quickreply.fragments.SettingsFragment
 
 class SettingsActivity : Activity() {
