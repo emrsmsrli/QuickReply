72424f5915f4973adce9c87a048f2e27b7b23cf0
Emre <emrsmsrli1@gmail.com>
2017-09-09 15:20:41 +0300

    make settingsActivity from support lib

--- app/src/main/java/tr/edu/iyte/quickreply/activities/SettingsActivity.kt
+++ app/src/main/java/tr/edu/iyte/quickreply/activities/SettingsActivity.kt
@@ -1,12 +1,12 @@
 package tr.edu.iyte.quickreply.activities
 
-import android.app.Activity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_settings.*
 import tr.edu.iyte.quickreply.R
 import tr.edu.iyte.quickreply.fragments.SettingsFragment
 
-class SettingsActivity : Activity() {
+class SettingsActivity : AppCompatActivity() {
     private val FRAGMENT_TAG = "settings"
 
     override fun onCreate(savedInstanceState: Bundle?) {
