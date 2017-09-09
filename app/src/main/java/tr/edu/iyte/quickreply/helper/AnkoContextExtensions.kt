d4530c17357588514724fa8779fd9db92afa64d7
Emre <emrsmsrli1@gmail.com>
2017-09-09 14:36:48 +0300

    new extensions for stopservice

--- app/src/main/java/tr/edu/iyte/quickreply/helper/AnkoContextExtensions.kt
+++ app/src/main/java/tr/edu/iyte/quickreply/helper/AnkoContextExtensions.kt
@@ -1,12 +1,16 @@
 package tr.edu.iyte.quickreply.helper
 
 import android.app.Activity
+import android.app.Fragment
 import android.app.Service
 import android.content.Context
 import android.content.Intent
 import android.service.quicksettings.TileService
+import android.support.v4.app.Fragment as SupportFragment
 
 inline fun <reified T: Service> Context.stopService() = this.stopService(Intent(this, T::class.java))
+inline fun <reified T: Service> Fragment.stopService() = activity.stopService(Intent(activity, T::class.java))
+inline fun <reified T: Service> SupportFragment.stopService() = activity.stopService(Intent(activity, T::class.java))
 
 inline fun <reified T: Activity> TileService.startActivityAndCollapse()
         = this.startActivityAndCollapse(Intent(this, T::class.java))
